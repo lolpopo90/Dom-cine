85a9fb4c218f3a15f2679db1524f2d33a3018541
lolpopo90 <doudou44hippopotame@gmail.com>
2026-01-16 08:42:24 +0100

    changement

--- java.js
+++ java.js
@@ -1,13 +1,13 @@
 function recherche(){
-    let recherche_film = document.getElementById("Recherche").value.toLowerCase().trim();// tolowercase cela met toute en minuscule et trim enleve les espace
+    let recherche_film = document.getElementById("Recherche").value.trim().toLowerCase();// tolowercase cela met toute en minuscule et trim enleve les espace
     
-    // Récupérer tous les divs contenant les films
+    // Récupérer tous les divs contenant les films (liste)
     let films = document.querySelectorAll('.image_rangement > div'); // je creer une liste avec toute les div qu'il y a dans la div avec la classe image_rangement
     
     films.forEach(film => { // Pour chaque div je fait tel action
         let titre = film.querySelector('figcaption').textContent.toLowerCase();// Récupérer le texte du figcaption (titre du film)
         // Afficher ou masquer selon si le titre commence par la recherche
-        if(recherche_film === "" || titre.startsWith(recherche_film)){ // les 2 barep parallele veut dire "OU"
+        if(recherche_film === "" || titre.startsWith(recherche_film)){ // les 2 bares parallele veut dire "OU"
             film.style.display = "block";//on decide de laisser afficher la div car soit la liste est vide donc on affiche tout soit on afiche car elle commence par ce qu'il y a dans la recherche
         } else {
             film.style.display = "none";// on decide de cacher la div du film
